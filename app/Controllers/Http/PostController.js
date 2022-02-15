451f15031006aeaacdd07c57714f220805a259fd
atrem13 <mertayoga17.my@gmail.com>
2022-02-15 16:04:24 +0800

    insert data done

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -3,7 +3,22 @@ const Post = use('App/Models/Post')
 class PostController {
     async index({ request, response, view}) {
         const posts = await Post.all()
-        return view.render('post.index', {posts: posts.rows})
+        return view.render('posts.index', {posts: posts.rows})
+    }
+
+    create({request, response, view}){
+        return view.render('posts.create')
+    }
+
+    async store({request, response, view, session}){
+        const post = new Post();
+        post.title = request.input('title')
+        post.content = request.input('content')
+
+        await post.save()
+
+      session.flash({ notification: 'New Post Added Successfully' })
+      return response.route('posts.index')
     }
 
 }
