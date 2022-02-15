637bad3a75ef24cd11b926ad7a155e05adeb9355
atrem13 <mertayoga17.my@gmail.com>
2022-02-15 15:55:42 +0800

    post index function done

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,6 +1,11 @@
 'use strict'
-
+const Post = use('App/Models/Post')
 class PostController {
+    async index({ request, response, view}) {
+        const posts = await Post.all()
+        return view.render('post.index', {posts: posts.rows})
+    }
+
 }
 
 module.exports = PostController
